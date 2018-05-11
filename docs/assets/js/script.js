fca0aff10b80c293ba880ba170aa6db7bde3b86c
Andre Monello <atmonello@gmail.com>
2018-05-10 22:16:29 -0300

    Adicionado carrosel de itens recomendados

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -24,12 +24,20 @@ getJSON = function(url) {
 getJSON('assets/js/products.json');
 console.log(data);
 
-for (var i = 0; i < data.widget.size; i++) {
-    var item = document.createElement('div');
-    item.className = 'item';
+for (var a = 0; a < data.widget.size; a++) {
+    var item = document.createElement('li');
+    item.className = 'item item-recommendation item-' + a;
+    item.innerHTML = '<div class="item-image"></div><h3 class="item-name"></h3><p class="item-old-price">De: <span class="value"></span></p><p class="item-price">Por: <span class="value"></span></p><p class="item-payment"><span class="value"></span></p>'
     var recommended = document.getElementsByClassName('items-list');
-    console.log(item);
-    console.log(recommended);
     recommended[0].appendChild(item);
 }
 
+var slider = tns({
+    container: '.items-list',
+    items: 3,
+    slideBy: 'page',
+    autoplay: false,
+    fixedWidth: 240,
+    controls: false,
+
+});
