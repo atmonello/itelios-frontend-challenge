15509d8d097d8ac1d0e7899d5705d183a45596f1
Andre Monello <atmonello@gmail.com>
2018-05-11 00:10:29 -0300

    Adicionados condicoes de pagamentos

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -35,12 +35,32 @@ viewedItemOldPrice.innerText = data.item.oldPrice;
 var viewedItemCurrentPrice = viewedItem[0].querySelector('.item-price .value');
 viewedItemCurrentPrice.innerText = data.item.price;
 
+var viewedItemPayment = viewedItem[0].querySelector('.item-payment');
+
+var viewedItemPaymentStrings = [
+    data.item.productInfo.paymentConditions.slice(0, (data.item.productInfo.paymentConditions.indexOf('é ') + 2)), 
+    data.item.productInfo.paymentConditions.slice((data.item.productInfo.paymentConditions.indexOf('é ') + 2), data.item.productInfo.paymentConditions.indexOf(' s')),
+    data.item.productInfo.paymentConditions.slice(data.item.productInfo.paymentConditions.indexOf(' s'), data.item.productInfo.paymentConditions.length)
+];
+
+// console.log(viewedItemPaymentStrings);
+
+for (var str = 0; str < viewedItemPaymentStrings.length; str++) {
+    if (str == 1) {
+        viewedItemPayment.innerHTML += '<span class="value">' + viewedItemPaymentStrings[str] + '</span>';
+        // console.log(viewedItemPaymentValue);
+        console.log(viewedItemPaymentStrings[str]);
+    }
+    else {
+        viewedItemPayment.innerHTML += viewedItemPaymentStrings[str];
+    }
+}
 
 for (var a = 0; a < data.widget.size; a++) {
     var item = document.createElement('li');
     item.className = 'item item-recommendation';
     item.id = a;
-    item.innerHTML = '<div class="item-image"></div><h3 class="item-name"></h3><p class="item-old-price">De: <span class="value"></span></p><p class="item-price">Por: <span class="value"></span></p><p class="item-payment"><span class="value"></span></p>'
+    item.innerHTML = '<div class="item-image"></div><h3 class="item-name"></h3><p class="item-old-price">De: <span class="value"></span></p><p class="item-price">Por: <span class="value"></span></p><p class="item-payment"></p>'
     var recommended = document.getElementsByClassName('items-list');
     recommended[0].appendChild(item);
 }
@@ -64,6 +84,26 @@ for (var b = 0; b < recommendedItems.length; b++) {
 
     var recommendedCurrentPrice = recommendedItems[b].querySelector('.item-price .value');
     recommendedCurrentPrice.innerText = data.recommendation[b].price;
+
+    var recommendedItemPayment = recommendedItems[b].querySelector('.item-payment');
+
+    var recommendedItemPaymentStrings = [
+        data.recommendation[b].productInfo.paymentConditions.slice(0, (data.recommendation[b].productInfo.paymentConditions.indexOf('é ') + 2)), 
+        data.recommendation[b].productInfo.paymentConditions.slice((data.recommendation[b].productInfo.paymentConditions.indexOf('é ') + 2), data.recommendation[b].productInfo.paymentConditions.indexOf(' s')),
+        data.recommendation[b].productInfo.paymentConditions.slice(data.recommendation[b].productInfo.paymentConditions.indexOf(' s'), data.recommendation[b].productInfo.paymentConditions.length)
+    ];
+
+    for (var str = 0; str < recommendedItemPaymentStrings.length; str++) {
+        if (str == 1) {
+            recommendedItemPayment.innerHTML += '<span class="value">' + recommendedItemPaymentStrings[str] + '</span>';
+            // console.log(viewedItemPaymentValue);
+            console.log(recommendedItemPaymentStrings[str]);
+        }
+        else {
+            recommendedItemPayment.innerHTML += recommendedItemPaymentStrings[str];
+        }
+    }
+
 }
 
 var slider = tns({
